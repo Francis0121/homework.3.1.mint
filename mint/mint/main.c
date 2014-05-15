eb87cb398cda9674fbc15fd9f06de8d200594cc3
Francis0121 <tjdrms0121@gmail.com>
2014-05-15 14:24:03 +0900

    Server 처

---
+++ mint/mint/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  mint
+//
+//  Created by 김세훈 on 2014. 5. 15..
+//  Copyright (c) 2014년 김세훈. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
---
+++ mint/mint/mint.h
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define TRUE 1
+#define FALSE 0
+
+typedef struct {
+    struct sockaddr_in addr,hAddr;
+    int socket;
+    char userId[100];
+    int isHost;
+} ClientInfo;
